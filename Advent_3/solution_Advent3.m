9d46e32a8ee5bee74ba53ac772a3ae02a953b4a0
Gordon_Surface <feld.gordon@gmail.com>
2020-12-10 16:55:14 +0100

    finished day 3

--- Advent_3/solution_Advent3.m
+++ Advent_3/solution_Advent3.m
@@ -2,20 +2,22 @@
 
 cd C:\GitHub_Repos\code_code_code\Advent_3
 
+input = char(importdata('puzzle_input.txt'));
+input_recoded = input == '#';
+
 down = [1 1 1 1 2];
-right = 3;
+right = [1 3 5 7 1];
 
 for slopes = 1:5
     
 
 start = 1;
 
-input = char(importdata('puzzle_input.txt'));
-input_recoded = input == '#';
 
-how_many_steps = ((size(input,1))-start)/down;
 
-how_wide = right*how_many_steps+1;
+how_many_steps = ((size(input,1))-start)/down(slopes);
+
+how_wide = right(slopes)*how_many_steps+1;
 multiplier = (round(1/(size(input,2)/how_wide),0))+1;
 
 input_long = repmat(input_recoded,1,multiplier);
@@ -24,16 +26,21 @@ T_input_long = input_long';
 
 width = size(T_input_long,1);
 
-position = start + down*width + right;
+position = start + down(slopes)*width + right(slopes);
 tree_counter = 0;
-for steps = 1:how_many_steps
+    for steps = 1:how_many_steps
 
-    tree_counter = tree_counter + T_input_long(position);
-%     [row(steps),col(steps)] = ind2sub([size(T_input_long,1) size(T_input_long,2)],position);
-    position = position + down*width + right;  
-    
-    
+        tree_counter = tree_counter + T_input_long(position);
+    %     [row(steps),col(steps)] = ind2sub([size(T_input_long,1) size(T_input_long,2)],position);
+        position = position + down(slopes)*width + right(slopes);  
+
+
+    end
+    slope_trees(slopes) = tree_counter;
+
 end
 
-end
+answer = prod(slope_trees);
+
+
     
