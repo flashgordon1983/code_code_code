da2949ac7cc53e502a5aa870fb3690c1171e3856
Gordon_Surface <feld.gordon@gmail.com>
2020-12-12 15:26:09 +0100

    read in data

--- Advent_4/solution_Advent_4.m
+++ Advent_4/solution_Advent_4.m
@@ -1,9 +1,19 @@
 
 
 input_file = fopen('example.txt','r');
-input = textscan(input_file,'%s', 'Delimiter',': ');
+stopper = 1;
+counter = 1;
+while stopper ~= -1
+    input{counter} = fgets(input_file);
+        if input{counter} == -1
+            stopper = -1;
+        end
+    counter = counter + 1;
+end
+fclose(input_file);
 
-input = fileread('example.txt');
+cellfun(@ischar,input);
 
-input = readtable('example.txt', 'ReadVariableNames', false);
+ischar(input{1});
 
+input{3};
