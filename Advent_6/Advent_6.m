d5a5ccc58fa2682de19931738f2ccfaea072e011
Gordon_Surface <feld.gordon@gmail.com>
2020-12-15 11:10:20 +0100

    solved_ 6_1

--- Advent_6/Advent_6.m
+++ Advent_6/Advent_6.m
@@ -23,43 +23,26 @@ for instr = 0:10
     catch
         break
     end
-% bags(:,3) = strcat(input(:,10),input(:,11));
-% bags(:,4) = strcat(input(:,14),input(:,15));
-% bags(:,5) = strcat(input(:,18),input(:,19));
 end
 
 bags = reshape(bags,[numel(bags) 1]);
 
 [unique_bags,bag_count,coded_bags] = unique(bags);
 
-coded_bags_formatted = reshape(coded_bags,[numel(coded_bags)/(instr+1) instr+1]);
+coded_bags_formatted = reshape(coded_bags,[numel(coded_bags)/(instr+1) instr+1 ]);
 
 my_code = coded_bags(contains(bags,'shinygold'));
 my_code = my_code(1);
 clear bags
 how_long = 1;
 counter = 0;
-my_new_code = [];
+all_codes = [];
 while how_long > 0
-%%%problem is that there are more levels to each bag!
-    for i = 1:numel(my_code)
-        index = (ismember(coded_bags_formatted,my_code));
-        index(:,1) = 0;
-        index1 = index;
-        index2 = index;
-        index2(:,2)=0;
-        index1(:,3) = 0;
-        counter = counter + sum(sum(index));
-        [row,col] = find(index1);
-        index1 = sub2ind([size(coded_bags_formatted,1)  size(coded_bags_formatted,2)], row, col-1);
-        [row,col] = find(index2);
-        index2 = sub2ind([size(coded_bags_formatted,1)  size(coded_bags_formatted,2)], row, col-2);
-        index = [index1 ; index2];
-        my_code = unique([my_new_code ;coded_bags_formatted(index)]);
-
-    end
-%     my_code = my_new_code;
-%     my_new_code = [];
+     index = (ismember(coded_bags_formatted,my_code));
+     index2 = sum(index(:,2:end)>0,2);
+      my_code = unique([my_new_code ;coded_bags_formatted(find(index2),1)]);
+      all_codes = [all_codes ; my_code];
+      counter = size(unique(all_codes),1);
     if isempty(my_code)
            break 
     end
